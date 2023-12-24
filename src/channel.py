66bc9e524afc3ce9b2b79beae727379908738bee
Marat Zaripov <maratos96@gmail.com>
2023-12-24 17:37:26 +0300

    homework 1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import os
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Channel:
@@ -5,8 +10,27 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = os.getenv('YOU_TUBE_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        request = self.youtube.channels().list(
+            part="snippet,statistics",
+            id=self.channel_id
+        )
+        response = request.execute()
+
+        channel_info = response.get('items', [])[0]
+        if channel_info:
+            snippet = channel_info.get('snippet', {})
+            statistics = channel_info.get('statistics', {})
+
+            print(f"Channel ID: {self.channel_id}")
+            print(f"Title: {snippet.get('title', 'N/A')}")
+            print(f"Description: {snippet.get('description', 'N/A')}")
+            print(f"Subscribers: {statistics.get('subscriberCount', 'N/A')}")
+            print(f"View Count: {statistics.get('viewCount', 'N/A')}")
+        else:
+            print("Unable to fetch channel information.")
