9192c0bc3b0a4d164c059a3cac5dbf0889369cca
Marat Zaripov <maratos96@gmail.com>
2023-12-27 18:37:47 +0300

    Исправления

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
@@ -14,31 +14,7 @@ class Channel:
         self.channel_id = channel_id
         self.api_key = os.getenv('YOU_TUBE_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        self.title = None
-        self.description = None
-        self.link = None
-        self.subscribers = None
-        self.videos = None
-        self.views = None
         self.update_info()
-
-    @classmethod
-    def get_service(cls):
-        return cls(channel_id="YOU_TUBE_API_KEY")
-
-    def to_json(self, filename):
-        data = {
-            "id": self.channel_id,
-            "title": self.title,
-            "description": self.description,
-            "link": self.link,
-            "subscribers": self.subscribers,
-            "videos": self.videos,
-            "views": self.views
-        }
-
-        with open(filename, 'w') as json_file:
-            json.dump(data, json_file, indent=2)
 
     def update_info(self):
         request = self.youtube.channels().list(
@@ -54,9 +30,29 @@ class Channel:
 
             self.title = snippet.get('title', 'N/A')
             self.description = snippet.get('description', 'N/A')
-            self.link = f"https://www.youtube.com/channel/{self.channel_id}"
+            self.url = f"https://www.youtube.com/channel/{self.channel_id}"
             self.subscribers = int(statistics.get('subscriberCount', 0))
-            self.videos = int(statistics.get('videoCount', 0))
-            self.views = int(statistics.get('viewCount', 0))
+            self.video_count = int(statistics.get('videoCount', 0))
+            self.view_count = int(statistics.get('viewCount', 0))
         else:
             print("Unable to fetch channel information.")
+
+    @classmethod
+    def get_service(cls):
+
+        real_channel_id = "UC-OVMPlMA3-YCIeg4z5z23A"
+        return cls(channel_id=real_channel_id)
+
+    def to_json(self, filename):
+        data = {
+            "id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers": self.subscribers,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file, indent=2)
